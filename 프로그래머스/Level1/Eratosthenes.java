ddb3d4f59d8a2ea0de3357193d228b1913c3c057
kceun <60288618+kceun@users.noreply.github.com>
2020-01-25 20:20:29 +0900

    Create Eratosthenes.java

---
+++ 프로그래머스/Level1/Eratosthenes.java
@@ -0,0 +1,26 @@
+class Solution {
+  public int solution(int n) {
+      int answer = 0;
+      
+		boolean[] check = new boolean[n+1];
+		
+		for(int i=2;i<=n;i++) {
+			if(check[i])
+				continue;
+			for(int j=i+i;j<=n;j+=i) {
+				check[j]=true;
+			}
+		}
+      
+      
+		for(int i=2;i<check.length;i++) {
+			if(!check[i])
+				answer++;
+			
+		}
+
+      return answer;
+      
+
+  }
+}
