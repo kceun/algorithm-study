9ae70e876db09407eeabd641a4e735e3571affdb
kceun <60288618+kceun@users.noreply.github.com>
2020-02-24 23:01:39 +0900

    SWEA no8016. 홀수 피라미드
    SWEA no8016. 홀수 피라미드

---
+++ SWEA/D3/no8016.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for(int tc=0;tc<T;tc++) {
+			long N = sc.nextInt();
+			long start=0,end=0;
+			start=(N-1)*(N-1)+(N-1)*(N-1)+1; //(N-1)^2 + 1 +(N-1)^2
+			end=(N*N)+(N*N)-1;				 //N^2 + N^2 -1
+			
+			System.out.println("#"+(tc+1)+" "+start+" "+end);
+			}
+	}
+}
