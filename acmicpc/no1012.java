6dd7db0e063da574cfddb333ed5cacf52b5c0ab9
kceun <60288618+kceun@users.noreply.github.com>
2020-03-01 17:12:46 +0900

    백준 1012번: 유기농 배추
    백준 1012번: 유기농 배추

---
+++ acmicpc/no1012.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static int T,N,M,K,ans = 0;
+	public static int[][] map;
+	public static boolean[][] visit;
+	public static int[] dx = {0,1,0,-1};//동남서북
+	public static int[] dy = {1,0,-1,0};//동남서북
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			ans = 0;
+			N = sc.nextInt(); 
+			M = sc.nextInt(); 
+			K = sc.nextInt();
+			map = new int[N][M];
+			visit = new boolean[N][M];
+			
+			for (int i = 0; i < K; i++) {
+				int row = sc.nextInt();
+				int col = sc.nextInt();
+				map[row][col]=1;
+			}
+			
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					if(map[i][j]==0)
+						continue;
+					if(visit[i][j])
+						continue;
+					
+					visit[i][j]=true;
+					dfs(i,j);
+					ans++;
+				}
+			}
+			
+			System.out.println(ans);
+		}			
+	}
+	
+	public static void dfs(int row, int col) {
+				
+
+		//방향 탐색
+		for (int i = 0; i < dx.length; i++) {
+			int nx = row + dx[i];
+			int ny = col + dy[i];
+			
+			//종료조건
+			//배열 범위 밖이냐?
+			if(nx >= N || ny >= M || nx < 0 || ny < 0 )
+				continue;
+			//배추가 없냐?
+			if(map[nx][ny]==0)
+				continue;
+			//이미 들른 곳이냐?
+			if(visit[nx][ny])
+				continue;
+			
+			visit[nx][ny]=true;
+			dfs(nx,ny);
+			
+		}
+	}
+
+}
