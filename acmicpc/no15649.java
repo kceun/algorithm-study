9d518238813975ae6b471d408e9b4290bc1cd1f5
kceun <60288618+kceun@users.noreply.github.com>
2020-03-03 13:57:41 +0900

    백준 15649번: N과 M (1)
    백준 15649번: N과 M (1)

---
+++ acmicpc/no15649.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Main {
+
+	public static int N,M;
+	public static int[] ans;
+	public static boolean[] visit;
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt(); //전체 수 1~N 
+		M = sc.nextInt(); //뽑을 갯수
+		
+		
+		for (int i = 1; i <= N; i++) {
+			ans = new int[M];
+			visit= new boolean[N+1];
+			int idx= 0;
+			ans[idx++]=i; //첫 시작점 넣어주기
+			
+			visit[i]=true;
+			dfs(idx);
+			visit[i]=false;
+		}
+	}
+	
+	public static void dfs( int idx) {
+		
+		//종료조건
+		//갯수 다 뽑았는가?
+		if(idx==M) {
+			for (int i = 0; i < M; i++) {
+				System.out.print(ans[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		
+		for (int i = 1; i <= N; i++) {
+			
+			//이미 뽑았던 숫자면 제외
+			if(visit[i])
+				continue;
+			
+			ans[idx]=i;
+			
+			visit[i]=true;
+			dfs(idx+1);
+			visit[i]=false;
+			
+			
+		}
+	}
+
+}
