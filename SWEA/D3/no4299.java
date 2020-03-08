37ab930897e3a984d6a0933ce84961fd58c35ca3
kceun <60288618+kceun@users.noreply.github.com>
2020-03-08 15:12:03 +0900

    SWEA no4299. 태혁이의 사랑은 타이밍
    SWEA no4299. 태혁이의 사랑은 타이밍

---
+++ SWEA/D3/no4299.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Solution
+{
+	public static int T, D,H,M,ans=0;
+	public static void main(String[] args)  {
+
+		Scanner sc = new Scanner(System.in);
+		
+		T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			D = sc.nextInt();
+			H = sc.nextInt();
+			M = sc.nextInt();
+			ans = 0;
+			
+			//소개팅 전에 차였냐?
+			if(D<11 || (D<=11 && H<11) || (D <= 11 && H <= 11 && D <11) ) {
+				System.out.println("#"+(tc+1)+" "+(-1));
+				continue;
+			}
+			
+			ans += ((D-11)*24*60);
+			ans += ((H-11)*60);
+			ans += (M-11);
+			
+			System.out.println("#"+(tc+1)+" "+ans);
+			
+		}
+	}
+}
