07168cb0d9409767fda4cb5ff1302103a3a22e1f
kceun <60288618+kceun@users.noreply.github.com>
2020-03-14 19:14:19 +0900

    SWEA - no8658. Summation
    SWEA - no8658. Summation

---
+++ SWEA/D3/no8658.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static int T;
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
+			for (int i = 0; i < 10; i++) {
+				String s = String.valueOf(sc.nextInt());
+				int sum = 0;
+				for (int j = 0; j < s.length(); j++) {
+					sum += Character.getNumericValue(s.charAt(j));
+				}
+				
+				max = Math.max(max, sum);
+				min = Math.min(min, sum);
+				
+			}
+			
+			System.out.println("#"+tc+" "+max+" "+min);
+		}
+	}
+
+}
