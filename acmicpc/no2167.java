ef61547f23011179f0ecaf025e218f92aaddcd19
kceun <60288618+kceun@users.noreply.github.com>
2020-03-14 20:13:18 +0900

    백준 2167번: 2차원 배열의 합
    백준 2167번: 2차원 배열의 합

---
+++ acmicpc/no2167.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+
+	public static int n,m,k,sum=0;
+	public static int[][] map;
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		map = new int[n+1][m+1];
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				map[i][j]=sc.nextInt();
+			}
+		}
+		
+		//합구하기
+		k = sc.nextInt();
+		for (int t = 0; t < k; t++) {
+			sum = 0;
+			int x1 = sc.nextInt();
+			int y1 = sc.nextInt();
+			int x2 = sc.nextInt();
+			int y2 = sc.nextInt();
+			
+			for (int i = x1; i <= x2; i++) {
+				for (int j = y1; j <= y2; j++) {
+					sum+=map[i][j];
+				}
+			}
+			
+			System.out.println(sum);
+		}
+
+	}
+
+}
