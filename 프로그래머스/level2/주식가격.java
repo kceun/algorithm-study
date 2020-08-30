241ab31bdaf500dc098570fb151f76274ba60552
kceun <60288618+kceun@users.noreply.github.com>
2020-08-30 15:19:31 +0900

    Create 주식가격.java

---
+++ 프로그래머스/level2/주식가격.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        
+        for(int i = 0 ; i<prices.length;i++) {
+            int cnt = 0;
+            for(int j = i+1 ; j < prices.length;j++) {
+                cnt++;
+                if(prices[i]>prices[j])
+                    break;
+            }
+            
+            answer[i]=cnt;
+        }
+        
+        // for(int i : answer ){
+        //     System.out.println(i);
+        // }
+        return answer;
+    }
+}
