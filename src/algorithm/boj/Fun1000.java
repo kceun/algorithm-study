e8148d0fa65d0eba6181d0547f58cf5d2b5450f7
CE <CE@DESKTOP-PO56S2V>
2020-09-04 14:03:08 +0900

    입출력문제 - A+B

---
+++ src/algorithm/boj/Fun1000.java
@@ -0,0 +1,16 @@
+package algorithm.boj;
+
+import java.util.Scanner;
+
+public class Fun1000 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(A+B);
+	}
+
+}
