7c316d9f61360dde6c857c4aeac2759ce69b023d
kceun <eun2ee@naver.com>
2020-09-04 14:49:55 +0900

    boj 입출력 A+B - 6

--- src/algorithm/boj/Fun10953.java
+++ src/algorithm/boj/Fun10953.java
@@ -1,14 +1,11 @@
 package algorithm.boj;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Fun10953 {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 		
-		System.setIn(new FileInputStream("input.txt"));
 		
 		Scanner sc = new Scanner(System.in);
 		
@@ -16,7 +13,6 @@ public class Fun10953 {
 		for (int i = 0; i < N; i++) {
 			String str = sc.next();
 			String[] sp = str.split(",");
-			
 			System.out.println(Integer.parseInt(sp[0])+Integer.parseInt(sp[1]));
 		}
 
