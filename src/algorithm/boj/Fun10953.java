38d191e8b70329b7e147e92bf9537e946acacb39
CE <CE@DESKTOP-PO56S2V>
2020-09-04 14:26:05 +0900

    BOJ 입출력 문제

---
+++ src/algorithm/boj/Fun10953.java
@@ -0,0 +1,24 @@
+package algorithm.boj;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Fun10953 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		System.setIn(new FileInputStream("input.txt"));
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			String str = sc.next();
+			String[] sp = str.split(",");
+			System.out.println(Integer.parseInt(sp[0])+Integer.parseInt(sp[1]));
+		}
+
+	}
+
+}
---
+++ src/algorithm/boj/Fun2558.java
@@ -0,0 +1,17 @@
+package algorithm.boj;
+
+import java.util.Scanner;
+
+public class Fun2558 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int A = Integer.parseInt(sc.next());
+		int B = Integer.parseInt(sc.next());
+		
+		System.out.println(A+B);
+	}
+
+}
