bbceb9687f66255285b38b6345c388bd651d4272
kceun <eun2ee@naver.com>
2020-09-05 12:35:05 +0900

    백준 2439번: 별 찍기 - 2 [입출력] -  Java

---
+++ src/algorithm/boj/Fun2439.java
@@ -0,0 +1,28 @@
+package algorithm.boj;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Fun2439 {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		System.setIn(new FileInputStream("input.txt"));
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for (int i = 1; i <=N; i++) {
+			
+			for (int j = 1; j <= N-i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
