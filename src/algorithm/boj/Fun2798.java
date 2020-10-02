63c3f6c53ba3f4e43d9178a4d88ed95565ce7f97
kceun <eun2ee@naver.com>
2020-10-02 21:54:50 +0900

    백준 2798번: 블랙잭 [브루트포스][BP] - Java

---
+++ src/algorithm/boj/Fun2798.java
@@ -0,0 +1,44 @@
+package algorithm.boj;
+
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Fun2798 {
+
+	public static int[] arr;
+	public static int N,M, ans=0;
+	public static void main(String[] args) throws IOException {
+
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				for (int k = 0; k < arr.length; k++) {
+					if(i!=j && j!=k && i!=k) {
+						int sum = arr[i]+arr[j]+arr[k];
+						if(sum<=M)
+							ans = Math.max(ans, sum);
+					}
+				}
+			}
+		}
+		System.out.println(ans);
+
+	}
+
+}
