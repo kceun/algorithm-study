cde05eafa11f0bc3aedbefe925b97bb22fbfc0f1
kceun <eun2ee@naver.com>
2020-10-02 21:41:22 +0900

    백준 4150번: 피보나치 수 - Java

---
+++ src/algorithm/boj/Fun4150.java
@@ -0,0 +1,33 @@
+package algorithm.boj;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Fun4150 {
+
+	public static BigInteger[] dp;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.setIn(new FileInputStream("input.txt"));
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		dp = new BigInteger[N+1];
+		
+		dp[0]=BigInteger.valueOf(0);
+		dp[1]=BigInteger.valueOf(1);
+		if(N>1)
+			dp[2]=BigInteger.valueOf(1);
+		
+		for (int i = 3; i <= N; i++) {
+			dp[i]=dp[i-1].add(dp[i-2]);
+		}
+		
+		System.out.println(dp[N]);
+		
+	}
+
+}
